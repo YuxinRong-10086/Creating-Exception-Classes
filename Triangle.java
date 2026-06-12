cb1114b55b5a8a4a5c55c0ec616f367bb1babb79
YuxinRong-10086 <evelynrong11@gmail.com>
2026-06-12 00:17:07 -0700

    Enhance Javadoc comments in Triangle class
    Updated Javadoc comments to provide detailed descriptions for methods and parameters.

--- Triangle.java
+++ Triangle.java
@@ -2,8 +2,9 @@ public class Triangle implements Shape{
         private double length;
 
     /**
-     *
-     * @param length
+     *Calculate the area of the equilateral triangle Object with the specific side
+     * @param length each side of the triangle
+     * @throws InvalidGeometricalDimensionException if length is less than or equalto zero
      */
     public Triangle(double length){
             this.length = length;
@@ -13,18 +14,18 @@ public class Triangle implements Shape{
         }
 
         /**
-         *
-         * @return
-        */
+     *Calculate the area of the equilateral triangle
+     * @param length the area of the triangle
+     */
     @Override
         public double getArea(){
             return length * Math.sqrt(3)/2;
         }
 
-        /**
-        *
-        * @return
-         */
+       /**
+         *Calculate the perimeter of the triangle
+         * @return the perimeter of the triangle
+        */
     @Override
         public double getPerimeter(){
             return length * 3;
