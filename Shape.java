edc5d311084640f7e6c2208e0fbc6a7e369c3066
YuxinRong-10086 <evelynrong11@gmail.com>
2026-06-12 00:21:59 -0700

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,6 +1,15 @@
+/**
+     *represents a geometric shape that can calculate shape and perimeter.
+     */
 public interface Shape{
-
+    /**
+     * Calculate the area of the shape
+     * @return the area
+     */
     double  getArea();
-
+    /**
+     * Calculate the perimeter of the shape
+     * @return the perimeter
+     */
     double getPerimeter();
 }
